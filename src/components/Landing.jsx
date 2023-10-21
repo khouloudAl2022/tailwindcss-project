0abba0731a6dac65b4915ef8c05652361d611527
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-10-21 20:23:42 +0100

    Style : Add margin and centered text button

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -15,13 +15,13 @@ const Landing = () => {
               All your files in one secure location, <br></br>
               accessible anywhere ,
             </h1>
-            <p className="font-normal text-lg px-[15px] md:w-[600px] max-w-full md:mx-auto">
+            <p className="font-normal text-lg px-[15px] md:w-[600px] max-w-full md:mx-auto mb-[30px]">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
               ipsum, cum dolor ut voluptates iste, nostrum atque incidunt
               reiciendis cupiditate of
             </p>
           </div>
-          <div>
+          <div className="btn w-[280px] h-[60px] rounded-[30px] text-center element-center mx-auto font-medium">
             <button>Get Started</button>
           </div>
         </div>
